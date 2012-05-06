286da23950dd4e93bfafec243cc5ce9f15ec6bc6
Daniel Diaz <diaz.carrete@facebook.com>
2012-05-06 14:08:26 -0700

    component types added

--- src/Main.hs
+++ src/Main.hs
@@ -26,11 +26,46 @@ import Network.MessagePackRpc.Client
 import Control.Concurrent
 import Control.Monad
  
+data ComponentType =
+     PanelComponent
+    |ButtonComponent T.Text
+    |TextFieldComponent T.Text
+    |OtherComponent T.Text
+    deriving Show
+
+instance Unpackable ComponentType where
+    get = undefined 
+
+instance Packable ComponentType  where
+    from _ = undefined
+
+instance OBJECT ComponentType where
+    toObject _ = undefined
+    tryFromObject (ObjectArray arr) =   
+        case arr of
+          [o1, o2] -> do
+            typeTag <- tryFromObject o1
+            case typeTag::Int of
+                1 -> return PanelComponent            
+                2 -> do 
+                    v2 <- tryFromObject o2
+                    return (ButtonComponent v2)
+                3 -> do
+                    v2 <- tryFromObject o2
+                    return (TextFieldComponent v2)
+                4 -> do
+                    v2 <- tryFromObject o2
+                    return (OtherComponent v2)
+                _ -> tryFromObjectError 
+          _ -> tryFromObjectError
+    tryFromObject _ = tryFromObjectError
+
 data Component = Component
     {
         _className::T.Text,
         _pos::(Int,Int),
         _dim::(Int,Int),
+        _componentType::ComponentType,
         _children::[Component]
     } deriving Show
 
@@ -44,12 +79,13 @@ instance OBJECT Component where
     toObject _ = undefined
     tryFromObject (ObjectArray arr) =   
         case arr of
-          [o1, o2, o3, o4] -> do
+          [o1, o2, o3, o4, o5] -> do
             v1 <- tryFromObject o1
             v2 <- tryFromObject o2
             v3 <- tryFromObject o3
             v4 <- tryFromObject o4
-            return (Component v1 v2 v3 v4)
+            v5 <- tryFromObject o5
+            return (Component v1 v2 v3 v4 v5)
           _ -> tryFromObjectError
     tryFromObject _ = tryFromObjectError
 
