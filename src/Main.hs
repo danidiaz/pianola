40fa3eea846b2b9ebc60c539e349a23033e1be92
Daniel Diaz <diaz.carrete@facebook.com>
2013-03-04 12:49:46 -0800

    reorganized test script

--- src/Main.hs
+++ src/Main.hs
@@ -27,6 +27,14 @@ checkStatusBar predicate = do
             pfail
     clickButtonByText (=="clear")
 
+checkDialog :: Monad m => Pianola m LogEntry (ComponentW m) ()
+checkDialog = do
+    clickButtonByText (=="open dialog")
+    with window $ with childWindow $ with contentsPane $ do
+        clickButtonByText (=="click this")
+        clickButtonByText (=="close dialog")
+    checkStatusBar (=="clicked button in dialog")
+
 type Test = Pianola Protocol LogEntry (GUI Protocol) ()
 
 testCase:: Test
@@ -36,71 +44,66 @@ testCase = with mainWindow $ do
         poke $ descendants >=> hasText (=="En un lugar de la Mancha") 
                            >=> setText "Lorem ipsum dolor sit amet"
         checkStatusBar (=="Lorem ipsum dolor sit amet")
-        poke $ descendants >=> hasText (=="open dialog") >=> click
-        with window $ with childWindow $ with contentsPane $ do
-            clickButtonByText (=="close dialog")
+        logmsg "testing dialog"
+        checkDialog
+        logmsg "dialog again, each action ralentized"
+        ralentize 2 $ checkDialog 
         logmsg "testing right click"
         rightClickByText (=="This is a label")
-        logmsg "finding popup..."
-        sleep 1
-        pmaybe pfail $ retryPoke1s 4 $ window >=> popupItem >=> hasText (=="popupitem2") >=> click  
+        pmaybe pfail $ retryPoke1s 4 $ 
+            window >=> popupItem >=> hasText (=="popupitem2") >=> click  
         checkStatusBar (=="clicked on popupitem2")
         sleep 1
-    logmsg "foo log message"
-    selectInMenuBar (Just True) $ map (==) ["Menu1","SubMenu1","submenuitem2"]
-    logmsg "getting a screenshot"
-    logcapture
-    logmsg "now for a second menu"
-    autolog $ selectInMenuBar Nothing $ map (==) ["Menu1","SubMenu1","submenuitem1"]
-    sleep 2
-    ralentize 2 $ with contentsPane $ do 
-        logmsg "ralentizing dialog-related actions by two seconds"
-        clickButtonByText (=="open dialog")
-        with window $ with childWindow $ with contentsPane $ do
-            clickButtonByText (=="click this")
-            clickButtonByText (=="close dialog")
-        checkStatusBar (=="clicked button in dialog")
-    logmsg "opening a file chooser"
-    with (contentsPane >=> descendants >=> hasText (=="Open file chooser")) $ do
-        poke click
-        with window $ with childWindow $ with contentsPane $ do
-            poke $ descendants >=> hasText (=="") >=> setText "/tmp/foofile.txt"   
-            clickButtonByText $ \txt -> or $ map (txt==) ["Open","Abrir"]
-        sleep 1
-    logmsg "working with a combo box"
-    with contentsPane $ with descendants $ do 
-        logmsg "this should show the combo"
-        selectInComboBox (=="ccc")
+        logmsg "foo log message"
+        with window $ selectInMenuBar (Just True) $ 
+            map (==) ["Menu1","SubMenu1","submenuitem2"]
+        logmsg "getting a screenshot"
+        with window $ logcapture
+        logmsg "now for a second menu"
+        autolog $ with window $ selectInMenuBar Nothing $ 
+            map (==) ["Menu1","SubMenu1","submenuitem1"]
         sleep 2
-        selectTabByText (=="tab two")  
-        sleep 2
-        poke $ \g -> do
-            Table ll <- return . cType $ g
-            cell <- replusify . concat $ ll
-            descendants . renderer >=> hasText (=="7") $ cell
-            return $ clickCell cell
-        sleep 2
-        poke $ \g -> do
-            Table ll <- return . cType $ g
-            cell <- replusify . concat $ ll
-            descendants . renderer >=> hasText (=="4") $ cell
-            return $ doubleClickCell cell
-        sleep 2
-        poke $ \g -> do    
-            Table {} <- return . cType $ g 
-            children >=> hasText (=="4") >=> setText "77" $ g
-        sleep 2
-        selectTabByText (=="tab JTree a")  
-        logmsg "tab change"
-        sleep 2
-        poke $ \g -> do    
-            Treegui forest <- return . cType $ g
-            cell <- replusify >=> descendants $ forest
-            descendants . renderer . rootLabel >=> hasText (=="leaf a") $ cell
-            (justZ . expand . rootLabel $ cell) <*> pure True
-        sleep 2
-        selectTabByText (=="labels")
+        logmsg "opening a file chooser"
+        with (descendants >=> hasText (=="Open file chooser")) $ do
+            poke click
+            with window $ with childWindow $ with contentsPane $ do
+                poke $ descendants >=> hasText (=="") >=> setText "/tmp/foofile.txt"   
+                clickButtonByText $ \txt -> or $ map (txt==) ["Open","Abrir"]
+            sleep 1
+        logmsg "working with a combo box"
+        with descendants $ do 
+            logmsg "this should show the combo"
+            selectInComboBox (=="ccc")
+            sleep 2
+            selectTabByText (=="tab two")  
+            sleep 2
+            poke $ \g -> do
+                Table ll <- return . cType $ g
+                cell <- replusify . concat $ ll
+                descendants . renderer >=> hasText (=="7") $ cell
+                return $ clickCell cell
+            sleep 2
+            poke $ \g -> do
+                Table ll <- return . cType $ g
+                cell <- replusify . concat $ ll
+                descendants . renderer >=> hasText (=="4") $ cell
+                return $ doubleClickCell cell
+            sleep 2
+            poke $ \g -> do    
+                Table {} <- return . cType $ g 
+                children >=> hasText (=="4") >=> setText "77" $ g
+            sleep 2
+            selectTabByText (=="tab JTree a")  
+            logmsg "tab change"
+            sleep 2
+            poke $ \g -> do    
+                Treegui forest <- return . cType $ g
+                cell <- replusify >=> descendants $ forest
+                descendants . renderer . rootLabel >=> hasText (=="leaf a") $ cell
+                (justZ . expand . rootLabel $ cell) <*> pure True
+            sleep 2
     with contentsPane $ do
+        with descendants $ selectTabByText (=="labels")
         sleep 1
         poke $ labeledBy (=="label2") >=> setText "hope this works!"
         sleep 2 
