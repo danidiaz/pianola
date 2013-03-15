d1d9879dd846a3cd2ed36917bbfedf374e08317c
Daniel Diaz <diaz.carrete@facebook.com>
2013-03-15 01:00:19 +0100

    refactored the driver action using an enum

--- backends/java-swing/src/main/java/info/danidiaz/pianola/driver/Driver.java
+++ backends/java-swing/src/main/java/info/danidiaz/pianola/driver/Driver.java
@@ -7,8 +7,12 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.imageio.ImageIO;
 
@@ -76,11 +80,173 @@ public class Driver implements Runnable
         this.messagePack = messagePack;
         this.releaseIsPopupTrigger = releaseIsPopupTrigger;
     }
+    
+    private enum Action {
+        CLICK("click") {
+            @Override
+            public void unpackInvoke(Unpacker unpacker, Snapshot snapshot) throws IOException  {
+                int cId = unpacker.readInt();
+                snapshot.click(cId);                
+            }  },
+        DOUBLECLICK("doubleClick") {
+            @Override
+            public void unpackInvoke(Unpacker unpacker, Snapshot snapshot) throws IOException {
+                int cId = unpacker.readInt();
+                snapshot.doubleClick(cId);
+            }  },
+        RIGHTCLICK("rightClick") {
+            @Override
+            public void unpackInvoke(Unpacker unpacker, Snapshot snapshot) throws IOException {
+                int cId = unpacker.readInt();
+                snapshot.rightClick(cId);               
+            }  },
+        CLICKBUTTON("clickButton") {
+            @Override
+            public void unpackInvoke(Unpacker unpacker, Snapshot snapshot) throws IOException {
+                int buttonId = unpacker.readInt();
+                snapshot.clickButton(buttonId);
+            }  },
+        TOGGLE("toggle") {
+            @Override
+            public void unpackInvoke(Unpacker unpacker, Snapshot snapshot) throws IOException {
+                int buttonId = unpacker.readInt();
+                boolean targetState = unpacker.readBoolean();
+                snapshot.toggle(buttonId,targetState);                
+            }  },
+        CLICKCOMBO("clickCombo") {
+            @Override
+            public void unpackInvoke(Unpacker unpacker, Snapshot snapshot) throws IOException {
+                int buttonId = unpacker.readInt();
+                snapshot.clickCombo(buttonId);                
+            }  },
+        SETTEXTFIELD("setTextField") {
+            @Override
+            public void unpackInvoke(Unpacker unpacker, Snapshot snapshot) throws IOException {
+                int buttonId = unpacker.readInt();
+                String text = unpacker.readString();
+                snapshot.setTextField(buttonId,text);                
+            }  },
+        CLICKCELL("clickCell") {
+            @Override
+            public void unpackInvoke(Unpacker unpacker, Snapshot snapshot) throws IOException {
+                int componentId = unpacker.readInt();
+                int rowId = unpacker.readInt();
+                int columnId = unpacker.readInt();
+                snapshot.clickCell(componentId,rowId,columnId);               
+            }  },
+        DOUBLECLICKCELL("doubleClickCell") {
+            @Override
+            public void unpackInvoke(Unpacker unpacker, Snapshot snapshot) throws IOException {
+                int componentId = unpacker.readInt();
+                int rowId = unpacker.readInt();
+                int columnId = unpacker.readInt();
+                snapshot.doubleClickCell(componentId,rowId,columnId);                
+            }  },
+        RIGHTCLICKCEll("rightClickCell") {
+            @Override
+            public void unpackInvoke(Unpacker unpacker, Snapshot snapshot) throws IOException {
+                int componentId = unpacker.readInt();
+                int rowId = unpacker.readInt();
+                int columnId = unpacker.readInt();
+                snapshot.rightClickCell(componentId,rowId,columnId);                
+            }  },
+        EXPANDCOLLAPSECELL("expandCollapseCell") {
+            @Override
+            public void unpackInvoke(Unpacker unpacker, Snapshot snapshot) throws IOException  {
+                int componentId = unpacker.readInt();
+                int rowId = unpacker.readInt();
+                boolean expand = unpacker.readBoolean();
+                snapshot.expandCollapseCell(componentId,rowId,expand);                
+            }  },
+        SELECTTAB("selectTab") {
+            @Override
+            public void unpackInvoke(Unpacker unpacker, Snapshot snapshot) throws IOException  {
+                int componentId = unpacker.readInt();
+                int tabid = unpacker.readInt();
+                snapshot.selectTab(componentId,tabid);               
+            }  },
+        GETWINDOWIMAGE("getWindowImage") {
+            @Override
+            public void unpackInvokePack(Unpacker unpacker, Snapshot snapshot,
+                    ByteArrayOutputStream imageBuffer, Packer packer)
+                    throws Exception {
+                int windowId = unpacker.readInt();
+                BufferedImage image = snapshot.getWindowImage(windowId);
+                imageBuffer.reset();
+                ImageIO.write(image, "png", imageBuffer);
+                packer.write((int)0);
+                packer.write(imageBuffer.toByteArray());
+            } },
+        CLOSEWINDOW("closeWindow") {
+            @Override
+            public void unpackInvoke(Unpacker unpacker, Snapshot snapshot) throws IOException {
+                int windowId = unpacker.readInt();
+                snapshot.closeWindow(windowId);                
+            }  },
+        TOFRONT("toFront") {
+            @Override
+            public void unpackInvoke(Unpacker unpacker, Snapshot snapshot) throws IOException {
+                int windowId = unpacker.readInt();
+                snapshot.toFront(windowId);                
+            }  },
+        ESCAPE("escape") {
+            @Override
+            public void unpackInvoke(Unpacker unpacker, Snapshot snapshot) throws IOException {
+                int windowId = unpacker.readInt();
+                snapshot.escape(windowId);                
+            }  },
+        ENTER("enter") {
+            @Override
+            public void unpackInvoke(Unpacker unpacker, Snapshot snapshot) throws IOException {
+                int windowId = unpacker.readInt();
+                snapshot.enter(windowId);                
+            }  };
+        
+        private final String name;
 
+        private Action(String name) {
+            this.name = name;
+        }
+        
+        public String getName() {
+            return name;
+        }
+        
+        public void unpackInvokePack(
+                Unpacker unpacker,
+                Snapshot snapshot,
+                ByteArrayOutputStream imageBuffer,
+                Packer packer
+            ) 
+        throws Exception {
+            try {
+                unpackInvoke(unpacker, snapshot);
+            } catch (Exception e) {
+                e.printStackTrace();
+                
+                packer.write((int)1); // An error happened.
+                packer.write((int)2); // Internal server error.
+                StringWriter sw = new StringWriter();
+                PrintWriter pw = new PrintWriter(sw);
+                packer.write(sw.toString());
+                return;
+            }
+            packer.write((int)0); // No error happened.
+            packer.writeNil();
+        }
+        
+        // For those requests which usually respond null
+        public void unpackInvoke(Unpacker unpacker,Snapshot snapshot) throws Exception { }
+    }
+    
     @Override
     public void run() {
         try {
-                       
+            Map<String,Action> actionMap = new HashMap<String,Action>();
+            for (Action a: Action.values()) {
+                actionMap.put(a.getName(),a);
+            }
+                
             boolean shutdownServer = false;
             while (!shutdownServer) {
                 Socket  clientSocket = serverSocket.accept();
@@ -93,136 +259,42 @@ public class Driver implements Runnable
                
                 try {
                     String methodName = unpacker.readString();                
-                    if (methodName.equals("snapshot")) {
+                    if (methodName.equals("shutdown")) {
+                        shutdownServer = true;
+                    } else if (methodName.equals("snapshot")) {
                         lastSnapshotId++;
                         Snapshot pianola = new Snapshot(lastSnapshot,releaseIsPopupTrigger);
-                        packer.write((int)0);
+                        packer.write((int)0); // No error happened.
                         pianola.buildAndWrite(lastSnapshotId,packer);
                         lastSnapshot = pianola;     
-                    } else if (methodName.equals("click")) {
+                    } else {
                         int snapshotId = unpacker.readInt();
-                        int cId = unpacker.readInt();
-                        lastSnapshot.click(cId);
-                        packer.write((int)0);
-                        packer.writeNil();
-                    } else if (methodName.equals("doubleClick")) {
-                        int snapshotId = unpacker.readInt();
-                        int cId = unpacker.readInt();
-                        lastSnapshot.doubleClick(cId);
-                        packer.write((int)0);
-                        packer.writeNil();
-                    } else if (methodName.equals("rightClick")) {
-                        int snapshotId = unpacker.readInt();
-                        int cId = unpacker.readInt();
-                        lastSnapshot.rightClick(cId);
-                        packer.write((int)0);
-                        packer.writeNil();
-                    } else if (methodName.equals("clickButton")) {
-                        int snapshotId = unpacker.readInt();
-                        int buttonId = unpacker.readInt();
-                        lastSnapshot.clickButton(buttonId);
-                        packer.write((int)0);
-                        packer.writeNil();
-                            
-                    } else if (methodName.equals("toggle")) {
-                        int snapshotId = unpacker.readInt();
-                        int buttonId = unpacker.readInt();
-                        boolean targetState = unpacker.readBoolean();
-                        lastSnapshot.toggle(buttonId,targetState);
-                        packer.write((int)0);
-                        packer.writeNil();                            
-                    }  else if (methodName.equals("clickCombo")) {
-                        int snapshotId = unpacker.readInt();
-                        int buttonId = unpacker.readInt();
-                        lastSnapshot.clickCombo(buttonId);
-                        packer.write((int)0);
-                        packer.writeNil();                            
-                    } else if (methodName.equals("setTextField")) {
-                        int snapshotId = unpacker.readInt();
-                        int buttonId = unpacker.readInt();
-                        String text = unpacker.readString();
-                        lastSnapshot.setTextField(buttonId,text);
-                        packer.write((int)0);
-                        packer.writeNil();
-                    }  else if (methodName.equals("clickCell")) {
-                        int snapshotId = unpacker.readInt();
-                        int componentId = unpacker.readInt();
-                        int rowId = unpacker.readInt();
-                        int columnId = unpacker.readInt();
-                        lastSnapshot.clickCell(componentId,rowId,columnId);
-                        packer.write((int)0);
-                        packer.writeNil();
-                    }  else if (methodName.equals("doubleClickCell")) {
-                        int snapshotId = unpacker.readInt();
-                        int componentId = unpacker.readInt();
-                        int rowId = unpacker.readInt();
-                        int columnId = unpacker.readInt();
-                        lastSnapshot.doubleClickCell(componentId,rowId,columnId);
-                        packer.write((int)0);
-                        packer.writeNil();
-                    }  else if (methodName.equals("rightClickCell")) {
-                        int snapshotId = unpacker.readInt();
-                        int componentId = unpacker.readInt();
-                        int rowId = unpacker.readInt();
-                        int columnId = unpacker.readInt();
-                        lastSnapshot.rightClickCell(componentId,rowId,columnId);
-                        packer.write((int)0);
-                        packer.writeNil();
-                    } else if (methodName.equals("expandCollapseCell")) {
-                        int snapshotId = unpacker.readInt();
-                        int componentId = unpacker.readInt();
-                        int rowId = unpacker.readInt();
-                        boolean expand = unpacker.readBoolean();
-                        lastSnapshot.expandCollapseCell(componentId,rowId,expand);
-                        packer.write((int)0);
-                        packer.writeNil();
-                    }  else if (methodName.equals("selectTab")) {
-                        int snapshotId = unpacker.readInt();
-                        int componentId = unpacker.readInt();
-                        int tabid = unpacker.readInt();
-                        lastSnapshot.selectTab(componentId,tabid);
-                        packer.write((int)0);
-                        packer.writeNil();
-                    }  else if (methodName.equals("getWindowImage")) {
-                        int snapshotId = unpacker.readInt();
-                        int windowId = unpacker.readInt();
-                        BufferedImage image = lastSnapshot.getWindowImage(windowId);
-                        imageBuffer.reset();
-                        ImageIO.write(image, "png", imageBuffer);
-                        packer.write((int)0);
-                        packer.write(imageBuffer.toByteArray());
-                    } else if (methodName.equals("closeWindow")) {
-                        int snapshotId = unpacker.readInt();
-                        int windowId = unpacker.readInt();
-                        lastSnapshot.closeWindow(windowId);
-                        packer.write((int)0);
-                        packer.writeNil();
-                    } else if (methodName.equals("toFront")) {
-                        int snapshotId = unpacker.readInt();
-                        int windowId = unpacker.readInt();
-                        lastSnapshot.toFront(windowId);
-                        packer.write((int)0);
-                        packer.writeNil();
-                    }  else if (methodName.equals("escape")) {
-                        int snapshotId = unpacker.readInt();
-                        int windowId = unpacker.readInt();
-                        lastSnapshot.escape(windowId);
-                        packer.write((int)0);
-                        packer.writeNil();
-                    } else if (methodName.equals("enter")) {
-                        int snapshotId = unpacker.readInt();
-                        int windowId = unpacker.readInt();
-                        lastSnapshot.enter(windowId);
-                        packer.write((int)0);
-                        packer.writeNil();
-                    } else if (methodName.equals("shutdown")) {
-                        shutdownServer = true;
+                        if (snapshotId == lastSnapshotId) {
+                            if (actionMap.containsKey(methodName)) {
+                                actionMap.get(methodName).unpackInvokePack(unpacker,
+                                        lastSnapshot,
+                                        imageBuffer,
+                                        packer
+                                    );
+                            } else {
+                                packer.write((int)1); // An error happened. 
+                                packer.write((int)2); // Server error. 
+                                packer.write("Unsupported method: " + methodName);
+                            }
+                        } else {
+                            packer.write((int)1); // An error happened. 
+                            packer.write((int)1); // Snapshot mismatch error. 
+                            packer.write((int)snapshotId);
+                            packer.write((int)lastSnapshotId); 
+                        }
                     }
                     sostream.flush();
                 } catch (IOException ioe) {
                     ioe.printStackTrace();    
                 } catch (MessageTypeException msgte) {                
                     msgte.printStackTrace();
+                } catch (Exception e) {
+                    e.printStackTrace();
                 } finally {
                     sistream.close();
                     sostream.close();
