a222edf8af27005f6ad06436055b41412aaf3360
Daniel Diaz <diaz.carrete@facebook.com>
2013-10-22 23:33:43 +0200

    changes to ruby script

--- run_tests.rb
+++ run_tests.rb
@@ -3,8 +3,9 @@ Dir.chdir(File.join('backends','java-swing')) {
 }
 Dir.chdir(File.join('backends','java-swing-testapp')) {
     puts `mvn install`
-    pid = Process::spawn "mvn exec:exec"
-    Process::detach pid
+    #pid = Process::spawn "mvn exec:exec"
+    pid = Process::spawn 'java -cp "..\java-swing\target\dependency\*;target\*" -javaagent:..\java-swing\target\pianola-driver-1.0.jar=port/26060,popupTrigger/release info.danidiaz.pianola.testapp.Main'
 }
 Process::exec 'cabal install --enable-tests'
 
+Process::kill(:SIGINT,pid)
