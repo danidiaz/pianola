256fb0c7e997c3db66ef6409924af7d910c79b96
Daniel Diaz <diaz.carrete@facebook.com>
2013-11-05 23:39:28 +0100

    added main Module

---
+++ src/Pianola/Main.hs
@@ -0,0 +1,34 @@
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE RankNTypes #-}
+
+module Pianola.Main (
+    ) where
+
+import Prelude hiding (catch,(.),id)
+import Control.Category
+import Data.Functor.Identity
+import Data.Tree
+import Data.Foldable (toList)
+import Data.MessagePack
+import Data.Attoparsec.ByteString
+import Control.Lens
+import Control.Arrow
+import Control.Monad
+import Control.Comonad
+import Control.Comonad.Trans.Class
+import Control.Comonad.Trans.Env    
+import Control.Applicative
+import Control.Monad.Trans.Maybe
+import Control.Monad.Logic
+import Control.Monad.Error
+import Data.Aeson
+import qualified Data.Text as T
+import qualified Data.Text.Encoding as E
+import qualified Data.ByteString as B
+import qualified Data.ByteString.Lazy as BL
+import Pipes
+
+import Pianola.Internal
+
+
