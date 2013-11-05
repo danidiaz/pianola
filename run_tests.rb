6fd45797c5e1e5e409e8afee624a9cc1dd1a5d52
Daniel Diaz <diaz.carrete@facebook.com>
2013-11-05 21:11:27 +0100

    stuff

--- run_tests.rb
+++ run_tests.rb
@@ -1,11 +1,15 @@
 Dir.chdir(File.join('backends','java-swing')) {
     puts `mvn install`
 }
+
 Dir.chdir(File.join('backends','java-swing-testapp')) {
     puts `mvn install`
     #pid = Process::spawn "mvn exec:exec"
     pid = Process::spawn 'java -cp "..\java-swing\target\dependency\*;target\*" -javaagent:..\java-swing\target\pianola-driver-1.0.jar=port/26060,popupTrigger/release info.danidiaz.pianola.testapp.Main'
 }
+
 Process::exec 'cabal install --enable-tests'
 
 Process::kill(:SIGINT,pid)
+
+# java -cp target\* info.danidiaz.pianola.robot.Main
