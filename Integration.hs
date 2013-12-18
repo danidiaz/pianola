ba5cda84c6feb9115289d0ee35888a9474fa3724
Daniel Diaz <diaz.carrete@facebook.com>
2013-12-18 12:18:31 +0100

    yak shaving

--- Integration.hs
+++ Integration.hs
@@ -1,13 +1,15 @@
-import Data.List
-import Data.Foldable
 import System.Exit
 import System.Exit.Lens
 import System.Process
 import System.FilePath
 import System.Directory
 
+import Data.List
+import Data.Foldable
+
 import Control.Lens
 import Control.Concurrent
+import Control.Applicative
 import Options.Applicative as O
 
 withDirectory :: FilePath -> IO a -> IO a
@@ -15,8 +17,14 @@ withDirectory path action = do
     current <- getCurrentDirectory 
     setCurrentDirectory path *> action <* setCurrentDirectory current
 
+withProcess :: IO ProcessHandle -> IO a -> IO a
+withProcess launch action = do
+    handle <- launch
+    action <* terminateProcess handle
+
 invoke :: FilePath -> [String] -> IO ()
 invoke file args = do 
+    putStrLn $ showCommandForUser file args
     (exitCode,stdout,stderr) <- readProcessWithExitCode file args ""
     putStrLn stdout
     putStrLn stderr
@@ -39,28 +47,26 @@ main = do
     b <- O.execParser $ O.info (O.helper <*> parser) O.fullDesc
     let os = if b then Windows else Linux 
         agentfolder = joinPath ["backends", "java-swing"]
-        threeUp = replicate 3 ".." 
-        classpath = intercalate ";" . map joinPath $  
-            [ threeUp ++ ["backends","java-swing","target","dependency","*"] 
-            , ["target","*"] 
-            ]
-        agentpath = joinPath $ threeUp ++
-            ["backends","java-swing","target","pianola-driver-1.0.jar"]
+        agentpath = joinPath [agentfolder,"target","pianola-driver-1.0.jar"]
         agentargs = (++) "=port/26060,popupTrigger/" $ case os of
             Windows -> "release"
             Linux   -> "press"
         agentclass = "info.danidiaz.pianola.testapp.Main"
         appfolder = joinPath ["integration","apps","java-swing-testapp"]
+        classpath = intercalate ";" . map joinPath $  
+            [ [agentfolder,"target","dependency","*"] 
+            , [appfolder,"target","*"] 
+            ]
     withDirectory agentfolder   $ maven 
     withDirectory appfolder     $ maven
     withDirectory "."           $ cabal True 
     withDirectory "integration" $ cabal False
-    handle <- withDirectory appfolder $ 
-        spawnProcess "java" ["-cp", classpath
-                            ,"-javaagent:" ++ agentpath ++ agentargs
-                            , agentclass
-                            ]  
-    threadDelay $ 10^6*2
-    invoke "pianola-integration-test-app" []
-    terminateProcess handle
+    let app = spawnProcess "java" 
+            ["-cp", classpath
+            ,"-javaagent:" ++ agentpath ++ agentargs
+            , agentclass
+            ]  
+    withProcess app $ do   
+        threadDelay $ 10^6*2
+        invoke "pianola-integration-test-app" []
     putStrLn "PASS" 
