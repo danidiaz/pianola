827f5f37dde1d436f1c98ae6a190024dee37f334
Daniel Diaz <diaz.carrete@facebook.com>
2013-12-19 15:10:48 +0100

    reworked optparser

--- src/Pianola/Main.hs
+++ src/Pianola/Main.hs
@@ -11,6 +11,7 @@ import Control.Category
 import Data.Functor.Identity
 import Data.Tree
 import Data.Foldable (toList)
+import Data.Monoid
 import Data.MessagePack
 import Data.Attoparsec.ByteString
 import Control.Lens
@@ -28,16 +29,68 @@ import qualified Data.Text as T
 import qualified Data.Text.Encoding as E
 import qualified Data.ByteString as B
 import qualified Data.ByteString.Lazy as BL
+import qualified Options.Applicative as O
 import Pipes
 
 import Pianola.Internal
 
-data Opts = Opts
-    { host :: Integer
-    , port :: String
-    , testName :: String
-    , listTests :: Bool
-    }
+data Command = ListC  
+             | RunC Run
+    deriving Show
+ 
+data Run = Run 
+        { testName :: String
+        , hostName :: String
+        , port :: Int
+        } 
+    deriving Show
+
+-- If using O.subparser instead of O.hsubparser, 
+-- you'll need to add O.helper to each sub-command parser.
+parser :: O.ParserInfo Command
+parser = O.info (O.helper <*> O.hsubparser subParsers) parserModifiers
+    where 
+    subParsers = listCommand <> runCommand 
+    listCommand = O.command "list" (O.info (pure ListC) listModifiers)
+    listModifiers = mconcat
+        [ O.fullDesc 
+        , O.header "This is the header" 
+        , O.footer "This is the footer"
+        , O.progDesc "This is the program description"
+        ] 
+    runCommand = O.command "run" (O.info (RunC <$> runParser) runModifiers)
+    runParser = Run <$> testName <*> machineOption <*> portOption
+    testName = O.argument O.str $ mconcat 
+        [ O.metavar "TEST"
+        , O.help "Name of the text to execute"
+        ]
+    machineOption = O.strOption $ mconcat
+        [ O.value "localhost" 
+        , O.showDefault 
+        , O.short 'm' 
+        , O.metavar "MACHINE" 
+        , O.help "GUI agent location"
+        ] 
+    portOption = O.option $ mconcat
+        [ O.value 26060
+        , O.showDefault
+        , O.short 'p' 
+        , O.metavar "PORT"
+        , O.help "GUI agent listening port"
+        ]   
+    runModifiers = mconcat
+        [ O.header "blah blach"
+        , O.footer "footer for run"
+        ]
+    parserModifiers = mconcat
+        [ O.fullDesc 
+        , O.progDesc "main prog desc" 
+        , O.header "hello - a test for optparse-applicative"
+        ]
+
+
+--testCase:: Monad m => Remote m -> Pianola m LogEntry GUI () 
+--testCase p = with (decorate $ topLevel.folded) $ do
 
 makeMain :: IO ()
 makeMain = undefined
